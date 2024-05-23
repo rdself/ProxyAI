a1b5af5bd2c44b99c17c1b20b9e8c15758f5bd5e
R.D. <rogerproself@gmail.com>
2024-05-22 19:50:50 -0700

    u

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Dooy/chatgpt-web-midjourney-proxy/main/src/assets/avatar.jpg',
-      name:  t('mjset.sysname'),//'AI绘图',
-      description: 'Star on <a href="https://github.com/Dooy/chatgpt-web-midjourney-proxy" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://raw.githubusercontent.com/RogerProSelf/ProxyGPT/main/src/assets/avatar.jpg',
+      name:  t('mjset.sysname'),
+      description: 'Star on <a href="https://github.com/RogerProSelf/ProxyGPT" class="text-blue-500" target="_blank" >GitHub</a>',
     },
   }
 }
