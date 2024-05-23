a991f90b94f3c64afdf5be23bb00484715acf14a
R.D. <rogerproself@gmail.com>
2024-05-22 17:55:55 -0700

    Update aiModel.vue

--- src/views/mj/aiModel.vue
+++ src/views/mj/aiModel.vue
@@ -94,14 +94,7 @@ const saveChat=(type:string)=>{
  
 watch(()=>nGptStore.value.model,(n)=>{
     nGptStore.value.gpts=undefined;
-    let max=4096;
-    if( n.indexOf('vision')>-1){
-        max=4096;
-    }else if( n.indexOf('gpt-4')>-1 ||  n.indexOf('16k')>-1 ){ //['16k','8k','32k','gpt-4'].indexOf(n)>-1
-        max=4096*2;
-    }else if( n.toLowerCase().includes('claude-3') ){
-         max=4096*2;
-    }
+    let max=64000;
     config.value.maxToken=max/2;
     if(nGptStore.value.max_tokens> config.value.maxToken ) nGptStore.value.max_tokens= config.value.maxToken;
 })
