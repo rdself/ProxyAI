d69f1f67125c1e7288c12b0dd51f09976daae064
R.D. <rogerproself@gmail.com>
2024-05-23 18:28:04 -0700

    u

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,7 +15,7 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://cdn.jsdelivr.net/gh/Siriling/my-icons@main/dist/icon/ChatGPT_A.png',
+      avatar: 'https://cdn.jsdelivr.net/gh/Siriling/my-icons@main/dist/icon/ChatGPT_B.png',
       name:  t('mjset.sysname'),
       description: 'By Roger',
     },
