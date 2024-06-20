64d318027285a026cfb265ca152e77c4ed779bb3
R.D. <rogerproself@gmail.com>
2024-06-19 19:35:43 -0700

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,7 +15,7 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://cdn.jsdelivr.net/gh/Siriling/my-icons@main/dist/icon/ChatGPT_B.png',
+      avatar: 'https://nas.deng.pub:15245/d/refer/stable/chatgpt.png',
       name:  t('mjset.sysname'),
       description: 'By Roger',
     },
